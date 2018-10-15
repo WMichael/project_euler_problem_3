52497d8917e21f18d8aa92260741232d83da76a2
Michael Weller <vve11ermichae1@gmail.com>
2018-10-15 19:55:18 +0100

    Created method for checking if a number is prime or not

--- lib/problem_3.rb
+++ lib/problem_3.rb
@@ -1,3 +1,23 @@
 class Problem_3
 
+  def prime? num
+    # If 0,1 or 2 then it's a prime.
+    if num == 0 || num == 1 || num == 2
+      true
+    # If it can be divided by 2 then it't not a prime.
+    elsif num % 2 != 0
+      # Loop from 3 up to number, if number / i gives no remainder then the number is not prime.
+      prime = true
+      for i in (3...num) do
+        if num % i == 0
+          prime = false
+        end
+      end
+      # If prime variable never gets set to false then it is true.
+      prime
+    else
+      false
+    end
+  end
+
 end
--- spec/problem_3_spec.rb
+++ spec/problem_3_spec.rb
@@ -1,1 +1,21 @@
 require_relative "../lib/problem_3.rb"
+
+describe "Problem 3" do
+  before(:all) do
+    @problem3 = Problem_3.new
+  end
+
+  it "Should check whether prime method is present" do
+    expect(@problem3).to respond_to :prime?
+  end
+
+  it "Should check whether a number is prime" do
+    expect(@problem3.prime? 3).to eq true
+    expect(@problem3.prime? 4).to eq false
+    expect(@problem3.prime? 2).to eq true
+    expect(@problem3.prime? 9).to eq false
+    expect(@problem3.prime? 15).to eq false
+    expect(@problem3.prime? 17).to eq true
+  end
+
+end
